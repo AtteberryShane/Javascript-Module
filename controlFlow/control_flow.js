9e57565e7e89a81e1fbcaa1a9564549989e3c991
AtteberryShane <shanecatteberry@gmail.com>
2026-02-25 13:19:50 -0600

    Control Flow and Conditional Statements lab commit

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,50 @@
+// Control Flow and Conditional Statements in  Javascript
+// Using if-else statements
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+
+// If-else statements using boolean variables
+let isLogginIn = true;
+let userMessage;
+
+if (isLogginIn) {
+    if(userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+console.log("User Message:", userMessage);
+
+// Control Flow Excercise
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unkown";
+}
+
+console.log("User Category:", userCategory);
